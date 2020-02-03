3521f26cff7315cdfccce0c21429f14a0cf72f10
Alan <29168822+0x416c616e@users.noreply.github.com>
2020-02-03 04:40:31 -0600

    made simple rootkit finder script

---
+++ rootkit_finder.sh
@@ -0,0 +1,98 @@
+#!/bin/bash
+#must be run as root
+divider() {
+    echo "======================="
+}
+
+#to-do: use tee to log to a file
+#call it 
+#to-do: check history of non-root users
+#to-do:
+#https://www.computerhope.com/unix/uat.htm
+#to-do: fg and bg tasks
+
+
+echo -n "checking for rootkits on "
+date
+divider
+echo "Part 1: environment"
+divider
+echo "PATH:"
+echo $PATH
+divider
+echo "aliases:"
+alias
+divider
+echo "Part 2: reading shell history"
+divider
+echo "history containing /etc/passwd:"
+history | grep /etc/passwd
+divider
+echo "history containing /etc/shadow:"
+history | grep /etc/shadow
+divider
+divider
+echo "history containing bash (sometimes used for bind shells/reverse shells):"
+history | grep bash
+divider
+echo "history containing /dev/tcp/ (sometimes used for reverse shells):"
+history | grep /dev/tcp/
+divider
+echo "history containing unset HISTFILE:"
+history | grep 'unset HISTFILE'
+divider
+echo "history containing nc:"
+history | grep nc
+divider
+echo "history containing netcat:"
+history | grep netcat
+divider
+echo "history containing dnscat:"
+history | grep dnscat
+divider
+echo "history containing wget:"
+history | grep wget
+divider
+echo "history containing curl:"
+history | grep curl
+divider
+echo "history containing cron:"
+history | grep cron
+divider
+echo "history containing systemctl:"
+history | grep systemctl
+divider
+echo "Part 2: logins"
+divider
+echo "currently logged in users:"
+w
+divider
+echo "last:"
+last
+divider
+echo "lastlog:"
+lastlog
+divider
+echo "current network connections (netstat -atu):"
+netstat -atu
+divider
+echo "Part 3: checking scheduled tasks"
+divider
+echo "systemd timers:"
+systemctl list-timers
+divider
+echo "/etc/crontab:"
+cat /etc/crontab
+divider
+echo "non-root user crontabs:"
+cat /var/spool/cron/crontabs/*
+divider
+echo "cron.daily"
+#to-do: anacron
+#to-do: cron.hourly
+#to-do: cron.weekly
+#to-do: cron.monthly
+#to-do: at
+#to-do: batch
+#to-do: atq
+#to-do: atrm
